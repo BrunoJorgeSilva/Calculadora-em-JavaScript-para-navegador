fa261ae7061e9842e72ba3a28f09ed93c9711381
isadorinha <war25war2525@gmail.com>
2022-03-10 15:03:57 -0300

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Como Utilizar:
+
+
+Baixar os Arquivos. Posteriormente extrair os mesmos dentro da pasta de sua preferência. Copiar o index.html para o navegador de sua preferência. 
---
+++ tiposdefuncao.js
@@ -0,0 +1,67 @@
+//tipos de funcoes
+
+//declarativas
+/*
+function funcao() {
+
+    console.log('tudo certo jovem')
+}
+
+funcao(); 
+
+//expressões de funções:
+
+var funcao = function funcao() {
+
+    console.log('mensagem automatica de função de exmpressão com nome de função')
+}
+funcao(); 
+
+
+var funcao = function() {
+
+    console.log('mensagem automática de função sem declarar o nome da função duas vezes!')
+}
+funcao(); 
+
+// Arrow function, quando a função não terá nome e apenas executará o que foi mandado, utilizando o simbolo => (que parece uma flecha por isso arrow function). 
+
+var funcao = () => {
+
+    console.log('olha só se não é ele, o arrow function')
+}
+funcao ()
+*/  
+
+/*só para salvar o código: 
+
+function calculadora() {
+    const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão Real (/)\n 5 - Divisão Inteira (%)\n 6 - potenciação (**)')); 
+
+let n1 = Number(prompt('Insira o primeiro valor:'));
+let n2 = Number(prompt('Insira o segundo valor:')); 
+let resultado;
+
+function soma() { 
+    resultado = n1 + n2;
+    alert('${n1} + ${n2} = ${resultado}')
+
+
+}
+if(operacao == 1) {
+    soma();
+} else if (operacao == 2) {
+    subtracao();
+} else if (operacao == 3) {
+    multiplicacao();
+} else if (operacao == 4) {
+    divisaoReal();
+} else if (operacao == 5) {
+    divisaoInteira();
+} else if (operacao == 6) {
+    potenciacao();
+}
+
+
+calculadora();
+}  */
